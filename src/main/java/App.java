e2ea526418d24dca30a0bffd96f2aecd22fffd63
VCHEPKOIT <vchepkoit@safaricom.co.ke>
2019-05-17 18:32:49 +0300

    decrypt file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,27 +1,53 @@
 import java.io.Console;
 
 public class App {
-
     public static void main(String[] args) {
+
+
+
         Console myConsole = System.console();
 
-        System.out.println("Input the message in plain text: ");
+        System.out.println("Input the plaintext message : ");
 
-        String plaintext = myConsole.readLine( );
+        String plaintext = myConsole.readLine();
 
-        System.out.println("Input the key (0-25): ");
+
+        System.out.println("Enter the value by which each character in the plaintext message gets shifted :");
 
         String shift = myConsole.readLine();
-
 
         int intShift = Integer.parseInt(shift);
 
 
-        CeaserCipher cipher = new CeaserCipher(plaintext, intShift);
-        cipher.encode(plaintext, intShift);
 
-        System.out.println("Your deciphered text is: " + cipher.ciphertext);
+        CeaserCipher cipher = new CeaserCipher( plaintext, intShift);
 
+        cipher.encode(plaintext , intShift);
+
+        System.out.println(" Your decoded text is : " + cipher.ciphertext);
+
+
+
+        System.out.println("Input the plaintext message : ");
+
+        String plaintext2 = myConsole.readLine();
+
+
+        System.out.println("Enter the value by which each character in the plaintext message gets shifted :");
+
+        String shift2 = myConsole.readLine();
+
+        int intShift2 = Integer.parseInt(shift2);
+
+
+
+        CeaserCipher2 decipher = new CeaserCipher2( plaintext2, intShift2);
+
+        decipher.setPlaintext2(plaintext2);
+        decipher.setIntShift2(intShift2);
+
+        decipher.decode(plaintext2 , intShift2);
+
+        System.out.println(" Your decoded text is : " + decipher.decryptMessage);
     }
-
 }
--- src/main/java/CeaserCipher.java
+++ src/main/java/CeaserCipher.java
@@ -1,2 +1,65 @@
+import java.util.*;
+
 public class CeaserCipher {
+
+    //instantiating
+    public String plaintext;
+    public int shift;
+    public String ciphertext = "";
+
+    public CeaserCipher(String plaintext, int shift) {
+        this.plaintext = plaintext;
+        this.shift = shift;
+    }
+
+    public int getShift() {
+        return shift;
+    }
+
+    public String getPlaintext() {
+        return plaintext;
+    }
+
+    public void setIntShift( int newIntShift) {
+        shift = newIntShift;
+    }
+
+    public void setPlaintext(String newPlaintext) {
+        plaintext = newPlaintext;
+    }
+
+    public String encode(String plaintext, int shift ) {
+
+
+        for(int i=0; i < plaintext.length();i++)
+        {
+            // Shift one character at a time
+            char alphabet = plaintext.charAt(i);
+
+            // if alphabet lies between a and z
+            if((alphabet >= 'a' && alphabet <= 'z') || (alphabet >= 'A' && alphabet <= 'Z'))
+            {
+                // shift alphabet
+                alphabet = (char) (alphabet + shift);
+            }
+            // if shift alphabet greater than 'z'
+
+            if(alphabet > 'z') {
+
+                // reshift to starting position
+                alphabet = (char) (alphabet+'a'-'z'-1);
+            }
+
+            // if shift alphabet greater than 'Z'
+            else if(alphabet > 'Z' && alphabet < 'a') {
+
+                //reshift to starting position
+                alphabet = (char) (alphabet+'A'-'Z'-1);
+            }
+            ciphertext = ciphertext + alphabet;
+        }
+        return  ciphertext;
+
+    }
+
 }
