a09187b70dabd2e7be377f3a27f021b45b3e14f1
VCHEPKOIT <vchepkoit@safaricom.co.ke>
2019-05-17 18:58:03 +0300

    cipher test code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,27 +27,5 @@ public class App {
         System.out.println(" Your decrypted text is : " + cipher.ciphertext);
 
 
-
-        System.out.println("Input the plaintext message : ");
-
-        String plaintext2 = myConsole.readLine();
-
-
-        System.out.println("Enter the value by which each character in the plaintext message gets shifted :");
-
-        String shift2 = myConsole.readLine();
-
-        int intShift2 = Integer.parseInt(shift2);
-
-
-
-        CeaserCipher2 decipher = new CeaserCipher2( plaintext2, intShift2);
-
-        decipher.setPlaintext2(plaintext2);
-        decipher.setIntShift2(intShift2);
-
-        decipher.decode(plaintext2 , intShift2);
-
-        System.out.println(" Your decoded text is : " + decipher.decryptMessage);
     }
 }
